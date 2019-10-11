5aef367d9a709bcc38bc81b1c13ea26ce9e716ba
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-10-11 23:00:24 +0800

    Initialize frontend project

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SplashComponent } from './splash/splash.component';
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },  
+  { path: 'home', component: HomeComponent },
+  { path: 'splash', component: SplashComponent }
+     
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
