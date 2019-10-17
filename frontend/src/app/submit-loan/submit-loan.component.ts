3f324190493d26ba3356267bf223abd519c87697
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-10-17 23:21:36 +0800

    Add in submit loan component

---
+++ frontend/src/app/submit-loan/submit-loan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-loan',
+  templateUrl: './submit-loan.component.html',
+  styleUrls: ['./submit-loan.component.css']
+})
+export class SubmitLoanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
