e361c9d530c0406b433f287a7aeb05742de1598c
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-10-18 15:12:59 +0800

    Add in submit loadn

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SplashComponent } from './splash/splash.component';
+import { SubmitLoanComponent } from './submit-loan/submit-loan.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'splash' },  
   { path: 'home', component: HomeComponent },
-  { path: 'splash', component: SplashComponent }
-     
+  { path: 'splash', component: SplashComponent },
+  { path: 'request', component: SubmitLoanComponent }
 ];
 
 @NgModule({
