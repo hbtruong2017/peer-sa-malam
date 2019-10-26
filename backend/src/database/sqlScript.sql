d50ae04cc5e7b7df98b2ecc85bc63f1324796277
Abel Wong <abel.wong.2017@sis.smu.edu.sg>
2019-10-26 13:17:03 +0800

    Update SQL Table

--- backend/src/database/sqlScript.sql
+++ backend/src/database/sqlScript.sql
@@ -8,8 +8,8 @@ DROP TABLE IF EXISTS `customerInfo`;
 
 create table loanDetails (
 	id INT primary key auto_increment,
-    borrowerId int not null,
-    loanerId int,
+    borrowerAccount int not null,
+    loanerAccount int,
     imgLink varchar(255) not null,
     loanCategory varchar(255) not null,
     loanDescription varchar(255) not null,
@@ -19,35 +19,39 @@ create table loanDetails (
 );
 
 create table customerInfo (
-	custId int primary key auto_increment,
+    accountNumber int primary key,
     custFirstName varchar(255) not null,
     custLastName varchar(255) not null,
     custRating float not null DEFAULT 0,
     jobTitle varchar(255) not null,
     companyName varchar(255),
     yearsInJob int,
-    accountNumber int not null unique,
     phoneNumber int not null,
     address varchar(255) not null
 );
 
 insert into loandetails
-	(borrowerId, imgLink,loanCategory, loanDescription, amount, interestRate, duration)
+	(borrowerAccount, imgLink,loanCategory, loanDescription, amount, interestRate, duration)
 values 
-	(1,"www.google.com","Vehicle","Buying a Ferrari",200.00,5.1,32),
-    (4,"www.google.com","Animal","Buying a Dragon",300.00,5.1,30),
-    (7,"www.google.com","Animal","Buying a Leviathon",600.00,5.1,2),
-    (10,"www.google.com","Human","Buying a Maid",7200.00,5.1,5),
-    (4,"www.google.com","Material","Buying  Gold",20.00,5.1,3);
+	(777,"www.google.com","Vehicle","Buying a Ferrari",200.00,5.1,32),
+    (666,"www.google.com","Animal","Buying a Dragon",300.00,5.1,30),
+    (777,"www.google.com","Animal","Buying a Leviathon",600.00,5.1,2),
+    (555,"www.google.com","Human","Buying a Maid",7200.00,5.1,5),
+    (444,"www.google.com","Material","Buying  Gold",20.00,5.1,3);
     
 insert into customerInfo
-	(custFirstName, custLastName, jobTitle, companyName, yearsInJob, accountNumber, phoneNumber, Address)
+	(accountNumber, custFirstName, custLastName, jobTitle, companyName, yearsInJob, phoneNumber, Address)
 values
-	("Jonah", "Hill", "Boss Baby", "",99, 456345,7485969,"21st Jump Street"),
-    ("Reynold" , "Ryan","Boss", "",9, 45345,7485349,"22nd Sit Street"),
-    ("Jonah","Hirola","Boss Baby", "",0, 456745,7485946,"40th dead Street"),
-    ("Jonah","Brody","Boss Baby", "",null, 426345,7235969,"666 Live Street"),
-    ("Jonah","Crinch", "Boss Baby", "",null, 459345,3847598,"777 Purge Street");
+	(777,"Jonah", "Hill", "Boss Baby", "",99,7485969,"21st Jump Street"),
+    (666,"Reynold" , "Ryan","Boss", "",9,7485349,"22nd Sit Street"),
+    (555,"Jonah","Hirola","Boss Baby", "",0,7485946,"40th dead Street"),
+    (444,"Jonah","Brody","Boss Baby", "",null,7235969,"666 Live Street"),
+    (333,"Jonah","Crinch", "Boss Baby", "",null, 459345,"777 Purge Street");
     
 
-select customerInfo.custId, loandetails.id, customerinfo.custFirstName, loandetails.borrowerId, loanerId, imgLink, amount, interestRate,duration from loanDetails inner join customerInfo on loanDetails.borrowerId = customerInfo.custId where 3 = customerInfo.custId;
+select customerInfo.accountNumber, loandetails.id, customerinfo.custFirstName, loandetails.borrowerAccount, loanerAccount, imgLink, amount, interestRate,duration from loanDetails inner join customerInfo on loanDetails.borrowerAccount = customerInfo.accountNumber where 444 = customerInfo.accountNumber;
+
+INSERT INTO customerInfo (accountNumber, custFirstName, custLastName, jobTitle, companyName, yearsInJob, phoneNumber, Address) VALUES( ) ON DUPLICATE KEY UPDATE    
+custFirstName ="abc", custLastName = "def" , jobTitle = "ghi", companyName="asd", yearsInJob = 9, phoneNumber=66666, Address = "latvia";
+
+select * from customerInfo;
