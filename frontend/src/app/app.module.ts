501e316b7ad32cbac051a30cdd7bdfb5633eead1
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-11-10 14:45:18 +0800

    Remove header component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SplashComponent } from './splash/splash.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -15,7 +14,6 @@ import { HistoryComponent } from './history/history.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
     SplashComponent,
     SubmitLoanComponent,
