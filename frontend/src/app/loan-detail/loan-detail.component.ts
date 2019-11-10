4f6bd7d8873fa122bad666f71548cbb16e0f4a0a
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-11-10 11:28:27 +0800

    Update service file

--- frontend/src/app/loan-detail/loan-detail.component.ts
+++ frontend/src/app/loan-detail/loan-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-loan-detail',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loan-detail.component.css']
 })
 export class LoanDetailComponent implements OnInit {
+  loanId: number;
+  loanInfo: any;
 
-  constructor() { }
+  constructor(private dataService: DataService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.loanId = params['id']
+    })
+    this.dataService.getLoanInfoByLoanId(this.loanId).subscribe((data: any) => {
+      this.loanInfo = data.loanInfo[0];
+      console.log(this.loanInfo)
+    })
   }
 
 }
